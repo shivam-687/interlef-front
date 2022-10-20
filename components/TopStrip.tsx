4e00f2ab4bc55d4136c6ff2e1c6636c8842c96db
Shivam Singh <shivamsi687@gmail.com>
2022-10-20 20:01:23 +0530

    update contact

--- components/TopStrip.tsx
+++ components/TopStrip.tsx
@@ -6,9 +6,9 @@ const TopStrip = () => {
     return (
         <div className='bg-primary text-primary-content py-1 bg-gradient-to-r from-primary via-[#EB12A0] to-primary'>
             <div className='flex items-center container mx-auto justify-between text-xs md:text-sm w-full'>
-                <span className='inline-flex items-center justify-center gap-2'> <MdCall className='text-lg' /> +91 798422623</span>
-                <span className='inline-flex items-center justify-center gap-2'> <FaWhatsapp className='text-lg' /> +91 798422623</span>
-                <span className='inline-flex items-center justify-center gap-2'> <MdEmail className='text-lg' /> shivam@interlef.com</span>
+                <a href='tel:7984226239' className='inline-flex items-center justify-center gap-2'> <MdCall className='text-lg' /> +91 7984226239</a>
+                <a href='https://wa.me/7984226239' className='inline-flex items-center justify-center gap-2'> <FaWhatsapp className='text-lg' /> +91 7984226239</a>
+                <a href="mailto:shivam@interlef.com" className='inline-flex items-center justify-center gap-2'> <MdEmail className='text-lg' /> shivam@interlef.com</a>
             </div>
         </div>
     )
