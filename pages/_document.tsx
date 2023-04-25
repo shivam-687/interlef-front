dfdde5563a49479795758e07a5f88be0cb8483cc
Shivam Singh <sjtsexperience@gmail.com>
2023-04-25 21:13:10 +0530

    adding ms-clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -71,6 +71,17 @@ class MyDocument extends Document {
                                })(window,document,'script','dataLayer','GTM-NMX5MPL');`
                         }
                     </Script>
+                    <Script id="clarity" strategy='lazyOnload'>
+                        {
+                            ` (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "gu4sjhv70x");`
+                        }
+                    </Script>
+
+
 
                     <meta name="robots" content="index, follow" />
                     <meta name="googlebot" content="index, follow" />
